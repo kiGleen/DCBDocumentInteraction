efee93c83b2fc561a34b4025e9699b9db1c96dfa
kiGleen <52207094+kiGleen@users.noreply.github.com>
2022-09-23 10:57:41 +0800

    Update AppDelegate.m

--- DCBDocumentInteraction/AppDelegate.m
+++ DCBDocumentInteraction/AppDelegate.m
@@ -2,8 +2,8 @@
 //  AppDelegate.m
 //  DCBDocumentInteraction
 //
-//  Created by zoujing@gogpay.cn on 2020/2/4.
-//  Copyright © 2020 cn.gogpay.dcb. All rights reserved.
+//  Created by zoujing on 2020/2/4.
+//  Copyright © 2020 gleen. All rights reserved.
 //
 
 #import "AppDelegate.h"
